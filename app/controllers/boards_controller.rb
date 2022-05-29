2b64315997631945acbcc32eaf5a3b5e935f828a
nonsuke0113 <asinal.iip36@icloud.com>
2022-05-29 13:48:37 +0900

    ページネーションの処理を追加。

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,9 @@
 class BoardsController < ApplicationController
+
+    before_action :set_target_board, only: %i[show edit update destroy]
+
     def index
-        @boards = Board.all
+        @boards = Board.page(params[:page])
     end
 
     def new
@@ -8,16 +11,35 @@ class BoardsController < ApplicationController
     end
 
     def create
-        Board.create(boards_params)
+        board = Board.create(board_params)
+        redirect_to board
     end
 
     def show
-        @board = Board.find(params[:id])
+    end
+
+    def edit
+    end
+
+    def update
+        @board.update(board_params)
+
+        redirect_to @board
+    end
+
+    def destroy
+        @board.delete
+
+        redirect_to boards_path
     end
 
     private
 
-    def boards_params
+    def board_params
         params.require(:board).permit(:name, :title, :body)
+    end
+
+    def set_target_board
+        @board = Board.find(params[:id])
     end
 end
