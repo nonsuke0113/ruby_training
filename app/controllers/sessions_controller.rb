b7982f44fe9afc21bec0d50fc9bad3e29ae2fdc3
nonsuke0113 <asinal.iip36@icloud.com>
2022-06-19 22:00:15 +0900

    Railsのバージョンアップに伴い、フォームを修正。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(name: params[:session][:name])
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to mypage_path
     else
